3def9f91d174b5325e17583eb89094800a6a4a1b
wj <wongjuanaa@gmail.com>
2025-10-24 18:02:32 -0400

    added some pages/images/text, basic navigation, simple theme, etc.

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="InterPrep",
+    layout="wide"
+)
+
+st.title("Welcome to InterPrep 💼")
+st.write("Prepare for your interviews with InterPrep.")
+
+st.image(
+    "https://www.blackstonetutors.com/wp-content/uploads/2024/04/Mock-Interview-1024x844.png",
+    width=650
+)
+
+st.write("TOTALLY NOT COPYING ")
+st.markdown("[https://www.blackstonetutors.com/university-of-pittsburgh-school-of-medicine-aamc-preview-test-questions/](https://www.blackstonetutors.com/university-of-pittsburgh-school-of-medicine-aamc-preview-test-questions/)")
---
+++ pages/about.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.header("About 🌟InterPrep🌟")
+
+## --
+st.write("""
+**InterPrep** helps you track and refine your interview preparation progress.
+""")
+
+st.write("""
+It's designed for simplicity and utility, helping job seekers practice verbally responding 
+to common behavioral (or technical) interview questions. It will **record**, **review**, and 
+**analyze** the responses based on the **STAR framework** (Situation, Task, Action, Result). 
+""")
+
+st.write("""
+It combines speech practice with data insights to make the interview preparation process 
+measurable and amenable to your dedication & self-improvement.
+""")
+
+
+## --
+st.subheader("Core Features")
+st.markdown(
+    """
+    - Record and replay responses to interview prompts  
+    - Track confidence and clarity over multiple attempts  
+    - Visualize weekly improvement trends  
+    - Organize practice prompts by **role**, **industry**, or **difficulty level**  
+    - Manage history of past sessions for focused review  
+    """
+)
+
+
+## --
+st.image(
+    "https://cdn.prod.website-files.com/6660a5bfdcf6c5fbf039f446/68da94f3b0778cb25e715f7d_67e1fa8f6200014e5c260a63_oupvlyty-each-box-represents-a-step-or-benefit-in-the-mock-interview-process-with-arrows-indicating-the-flow-of-how-these-steps-contribute-to-overall-success-in-job-searching.webp",
+    width=850
+)
+
+
