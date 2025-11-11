0ac42cdf07776eeebda2872fb2730a21b719d521
wj <wongjuanaa@gmail.com>
2025-11-11 02:35:15 -0500

    added code editor, audio recording components

--- pages/interview.py
+++ pages/interview.py
@@ -1,8 +1,11 @@
 import streamlit as st
+from code_editor import code_editor
 import random
+import os
 
 def render():
     st.header("2: Interview Question")
+    user_code_obj = None
 
     filtered_questions = st.session_state.get("filtered_questions", [])
     if not filtered_questions:
@@ -15,18 +18,49 @@ def render():
     st.markdown("#### Question:")
     st.write(st.session_state.current_question["question"])
 
-    st.info("Coding IDE")
-    st.info("Audio recording widget placeholder")
+    if "user_code_obj" in st.session_state:
+        user_code_obj = st.session_state["user_code_obj"]
+
+    col1, col2 = st.columns([1.5, 0.5])
+
+    # EMBEDDED CODING IDE
+    with col1:
+        # st.info("Coding IDE")
+        initial_code = '''def placeholder():\n    print("Hello World")'''
+        user_code = code_editor(initial_code, lang="python")
+        if user_code:
+            user_code_obj = user_code
+
+    # AUDIO - show TRANSCRIPT in Results
+    with col2:
+        # st.info("Audio recording widget placeholder")
+        # TODO: Whisper AI --
+        audio = st.audio_input("Record")
+        if audio:
+            st.audio(audio)  # playback
+            os.makedirs("audio", exist_ok=True)
+            # save option
+            num = random.randint(1000000, 9999999)
+            filename = f"audio/user_recorded_{num}.wav"
+            with open(filename, "wb") as f:
+                f.write(audio.getbuffer())
+            st.success(f"Saved! {filename}")
 
 
     # redirect: new question, results
-    col1, col2 = st.columns(2)
+    st.write("")
+    st.write("")
+    st.write("")
+    col1, spc, col2 = st.columns([1, 1, 1])
 
-    practice_new_clicked = col1.button("Practice New", key="practice_new_btn")
-    results_clicked = col2.button("View Results", key="results_btn")
+    practice_new_clicked = col1.button("Practice New", key="practice_new_btn", width='stretch')
+    results_clicked = col2.button("Submit & View Results", key="results_btn", width='stretch')
 
     if practice_new_clicked:
         st.session_state.page = "select_criteria"
+        st.rerun()
 
     if results_clicked:
+        st.session_state["user_code_obj"] = user_code_obj
         st.session_state.page = "results"
+        st.rerun()
--- pages/results.py
+++ pages/results.py
@@ -1,26 +1,47 @@
 import streamlit as st
 
+if "user_code_obj" not in st.session_state:
+    st.session_state["user_code_obj"] = None
+
 def render():
     st.header("3: Evaluation & Feedback")
 
-    code = st.text_area("Code Submission", st.session_state.get("code", ""), height=150)
-    st.session_state.code = code
+    col1, col2 = st.columns([1.5, 1])
+    with col1:
+        user_code_obj = st.session_state.get("user_code_obj", None)
+        if user_code_obj:
+            code_text = user_code_obj.get("text") if isinstance(user_code_obj, dict) else user_code_obj
+            st.code(code_text, language="python")
+            print("User code object:", user_code_obj)
+            print("Session state keys:", st.session_state.keys())
+            print("Current stored code:", st.session_state.get("user_code_obj"))
+        else:
+            st.info("No code submitted yet.")
 
-    transcript = st.text_area("Transcript", st.session_state.get("transcript", ""), height=150)
-    st.session_state.transcript = transcript
 
+    # Whisper AI
+    # transcript = st.text_area("Transcript", st.session_state.get("transcript", ""), height=150)
+    # st.session_state.transcript = transcript
+    with col2:
+        st.info("Whisper - Transcript placeholder")
+
+    st.divider()
     st.subheader("Feedback")
     st.write(st.session_state.get("feedback", "Feedback here."))
 
 
     # redirect: new question, dashboard
-    col1, col2 = st.columns(2)
+    st.write("")
+    st.divider()
+    col1, spc, col2 = st.columns([1, 1, 1])
 
-    practice_new_clicked = col1.button("Practice New", key="practice_new_btn")
-    dashboard_clicked = col2.button("Dashboard", key="dashboard_btn")
+    practice_new_clicked = col1.button("Practice New", key="practice_new_btn", width='stretch')
+    dashboard_clicked = col2.button("Dashboard", key="dashboard_btn", width='stretch')
 
     if practice_new_clicked:
         st.session_state.page = "select_criteria"
+        st.rerun()
 
     if dashboard_clicked:
         st.session_state.page = "dashboard"
+        st.rerun()
