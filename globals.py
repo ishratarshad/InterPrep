4e6980e047d63484ee8c906797459992fb48a405
wj <wongjuanaa@gmail.com>
2025-11-13 03:51:55 -0500

    add st_ace editor programming langs to global

--- globals.py
+++ globals.py
@@ -6,3 +6,81 @@ linkColor = "#388E3C"
 buttonBgColor = "#D9EAD3"
 buttonHoverColor = "#5AC639"
 buttonBorderColor = "#72D87B"
+
+ACE_LANG_OPTIONS = {
+    "Python": {
+        "mode": "python",
+        "extension": "py",
+        "placeholder": "def placeholder():\n    print('Hello World')"
+    },
+    "JavaScript": {
+        "mode": "javascript",
+        "extension": "js",
+        "placeholder": "function placeholder() {\n    console.log('Hello World');\n}"
+    },
+    "JSON": {
+        "mode": "json",
+        "extension": "json",
+        "placeholder": '{\n  "key": "value"\n}'
+    },
+    "HTML": {
+        "mode": "html",
+        "extension": "html",
+        "placeholder": "<!DOCTYPE html>\n<html>\n  <body>\n    <h1>Hello World</h1>\n  </body>\n</html>"
+    },
+    "CSS": {
+        "mode": "css",
+        "extension": "css",
+        "placeholder": "body {\n    background-color: #fff;\n    color: #333;\n}"
+    },
+    "Markdown": {
+        "mode": "markdown",
+        "extension": "md",
+        "placeholder": "# Hello World\nThis is a placeholder markdown text."
+    },
+    "C++": {
+        "mode": "c_cpp",
+        "extension": "cpp",
+        "placeholder": "#include<iostream>\nint main() {\n    std::cout << \"Hello World\";\n    return 0;\n}"
+    },
+    "Java": {
+        "mode": "java",
+        "extension": "java",
+        "placeholder": "public class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println(\"Hello World\");\n    }\n}"
+    },
+    "Ruby": {
+        "mode": "ruby",
+        "extension": "rb",
+        "placeholder": "def placeholder\n  puts 'Hello World'\nend"
+    },
+    "Go": {
+        "mode": "golang",
+        "extension": "go",
+        "placeholder": "package main\nimport \"fmt\"\nfunc main() {\n    fmt.Println(\"Hello World\")\n}"
+    },
+    "PHP": {
+        "mode": "php",
+        "extension": "php",
+        "placeholder": "<?php\necho 'Hello World';\n?>"
+    },
+    "Swift": {
+        "mode": "swift",
+        "extension": "swift",
+        "placeholder": "print(\"Hello World\")"
+    },
+    "TypeScript": {
+        "mode": "typescript",
+        "extension": "ts",
+        "placeholder": "function placeholder(): void {\n    console.log('Hello World');\n}"
+    },
+    "SQL": {
+        "mode": "sql",
+        "extension": "sql",
+        "placeholder": "SELECT 'Hello World';"
+    },
+    "Shell": {
+        "mode": "sh",
+        "extension": "sh",
+        "placeholder": "echo \"Hello World\""
+    }
+}
