e6b02ee54f404fc3f2b51391b9750d2895bf0877
wj <wongjuanaa@gmail.com>
2025-11-20 17:37:44 -0500

    update interviewing/programming langs

--- globals.py
+++ globals.py
@@ -25,26 +25,6 @@ ACE_LANG_OPTIONS = {
         "extension": "js",
         "placeholder": "function placeholder() {\n    console.log('Hello World');\n}"
     },
-    "JSON": {
-        "mode": "json",
-        "extension": "json",
-        "placeholder": '{\n  "key": "value"\n}'
-    },
-    "HTML": {
-        "mode": "html",
-        "extension": "html",
-        "placeholder": "<!DOCTYPE html>\n<html>\n  <body>\n    <h1>Hello World</h1>\n  </body>\n</html>"
-    },
-    "CSS": {
-        "mode": "css",
-        "extension": "css",
-        "placeholder": "body {\n    background-color: #fff;\n    color: #333;\n}"
-    },
-    "Markdown": {
-        "mode": "markdown",
-        "extension": "md",
-        "placeholder": "# Hello World\nThis is a placeholder markdown text."
-    },
     "C++": {
         "mode": "c_cpp",
         "extension": "cpp",
@@ -54,11 +34,6 @@ ACE_LANG_OPTIONS = {
         "mode": "java",
         "extension": "java",
         "placeholder": "public class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println(\"Hello World\");\n    }\n}"
-    },
-    "Ruby": {
-        "mode": "ruby",
-        "extension": "rb",
-        "placeholder": "def placeholder\n  puts 'Hello World'\nend"
     },
     "Go": {
         "mode": "golang",
@@ -80,14 +55,4 @@ ACE_LANG_OPTIONS = {
         "extension": "ts",
         "placeholder": "function placeholder(): void {\n    console.log('Hello World');\n}"
     },
-    "SQL": {
-        "mode": "sql",
-        "extension": "sql",
-        "placeholder": "SELECT 'Hello World';"
-    },
-    "Shell": {
-        "mode": "sh",
-        "extension": "sh",
-        "placeholder": "echo \"Hello World\""
-    }
 }
